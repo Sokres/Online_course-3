0cd493cb77037c6fa85bf554e40f3f9270868cb9
Sokres <fieldytr@gmail.com>
2023-08-16 13:51:44 +0500

    Страница Практикума по  произношению

--- gulp/clean_old.js
+++ gulp/clean_old.js
@@ -3,7 +3,8 @@ const del = require ('del');
 module.exports = function clean_old() {
   return del([
     'build/css/**/*',
-    '!build/css/{styles,marathom,print}-*.css',
+    // '!build/css/{styles,marathom,print}-*.css',
+    '!build/css/*.css',
     'build/rev.json',
 ]);
 }
